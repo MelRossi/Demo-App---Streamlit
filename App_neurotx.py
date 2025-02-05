ba69135b7a3a225e1fd70a21663804c86f1d59fc
MelRossi <melirossi.mr@gmail.com>
2025-02-04 21:15:04 -0300

    Update App_neurotx.py

--- App_neurotx.py
+++ App_neurotx.py
@@ -418,4 +418,4 @@ st.download_button(
             )   
 
 # Link archivo de análisis
-st.link_button(":page_facing_up: Descargar Archivo de Análisis", "https://github.com/MelRossi/App-Neuro-Tx-Latam/raw/main/An%C3%A1lisis_METS.ipynb")
+st.link_button(":page_facing_up: Descargar Archivo de Análisis", "https://github.com/MelRossi/Demo-App---Streamlit/blob/main/analisis%26modelos.pdf")
